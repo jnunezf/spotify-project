7f1ef37f7cb52d3f84f01ad63416765a109d323e
jnunezf <jnunezf@gmail.com>
2020-06-01 22:32:57 -0400

    Publishing ML Application to Gemfury 11

--- packages/ml_model/ml_model/processing/data_management.py
+++ packages/ml_model/ml_model/processing/data_management.py
@@ -19,7 +19,16 @@ def load_pipeline(*, filename: str) -> Pipeline:
 def save_pipeline(*, pipeline_to_persist) -> None:
 
     filename = f'ml_model_output_v{_version}.pkl'
-
     path = config.TRAINED_MODEL_DIR / filename
+
+    remove_old_pipelines(files_to_keep=filename)
+
     joblib.dump(pipeline_to_persist, path)
     _logger.info(f'\nsaved pipeline: {filename}')
+
+
+def remove_old_pipelines(*, files_to_keep) -> None:
+
+    for model_file in config.TRAINED_MODEL_DIR.iterdir():
+        if model_file.name not in [files_to_keep, "__init__.py"]:
+            model_file.unlink()
