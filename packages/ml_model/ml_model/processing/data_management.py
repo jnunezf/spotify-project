0a54e7e6236b8331d461702bc2b9ab30ed722e4c
jnunezf <jnunezf@gmail.com>
2020-06-02 23:31:07 -0400

    Differential test in CI Part2 2

--- packages/ml_model/ml_model/processing/data_management.py
+++ packages/ml_model/ml_model/processing/data_management.py
@@ -10,7 +10,7 @@ from ml_model import __version__ as _version
 _logger = logging.getLogger(__name__)
 
 def load_dataset(*, filename: str) -> pd.DataFrame:
-    _data = pd.read_csv(f"{config.DATASET_DIR}/{filename}", index_col=[0])
+    _data = pd.read_csv(f"{config.PACKAGE_ROOT}/{filename}", index_col=[0])
     return _data
 
 def load_pipeline(*, filename: str) -> Pipeline:
