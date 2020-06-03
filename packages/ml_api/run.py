a62f8b323241772682c9f4bce1db0521139ef228
jnunezf <jnunezf@gmail.com>
2020-06-03 18:24:21 -0400

    Heroku Deployment Config - diff-branch

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,8 +1,8 @@
 from api.app import create_ml_app
-from api.config import DevelopmentConfig
+from api.config import DevelopmentConfig, ProductionConfig
 
 app = create_ml_app(
-    config_object = DevelopmentConfig
+    config_object = ProductionConfig
 )
 
 if __name__ == '__main__':
